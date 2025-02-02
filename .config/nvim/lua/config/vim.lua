63046788007351007913846d32efaceec29b1c26
MarcioSobel <marcinhosobel@hotmail.com>
2025-02-02 03:29:24 -0300

    Update vim.lua

--- .config/nvim/lua/config/vim.lua
+++ .config/nvim/lua/config/vim.lua
@@ -7,23 +7,34 @@ o.showmode = false
 o.cursorline = true
 o.cursorlineopt = "number"
 
-opt.fillchars = { eob = " " }
 o.ignorecase = true
 o.smartcase = true
 o.mouse = "a"
 
-o.number = true
+o.nu = true
 o.relativenumber = true
 o.numberwidth = 2
+
+opt.fillchars = { eob = " " }
+
+opt.hlsearch = false
+opt.incsearch = true
+
+opt.scrolloff = 8
+
+opt.updatetime = 50
+opt.colorcolumn = { "80", "120" }
 
 -- go to previous/next line with h,l,left arrow and right arrow
 -- when cursor reaches end/beginning of line
 opt.whichwrap:append("<>[]hl")
 
 -- INDENTATION SETTINGS
-vim.o.tabstop = 4
-vim.o.shiftwidth = 4
-vim.o.expandtab = false
+o.tabstop = 4
+o.softtabstop = 4
+o.shiftwidth = 4
+o.expandtab = true
+o.smartindent = true
 
 local filetypes = { -- file types that should have 2 spaces indentation
 	"html",
@@ -34,7 +45,7 @@ vim.cmd("augroup IndentationSettings")
 vim.cmd("autocmd!")
 
 for _, filetype in ipairs(filetypes) do
-	vim.cmd("autocmd FileType " .. filetype .. " setlocal tabstop=2 shiftwidth=2 expandtab")
+	vim.cmd("autocmd FileType " .. filetype .. " setlocal tabstop=2 shiftwidth=2")
 end
 
 vim.cmd("augroup END")
