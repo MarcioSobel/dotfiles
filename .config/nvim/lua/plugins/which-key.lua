c284b1b653ab430cb820d86c5302e3573feb9a99
MarcioSobel <marcinhosobel@hotmail.com>
2025-02-02 03:30:30 -0300

    add which-key

---
+++ .config/nvim/lua/plugins/which-key.lua
@@ -0,0 +1,19 @@
+return {
+	{
+		"folke/which-key.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		event = "VeryLazy",
+		opts = {
+			preset = "helix",
+		},
+		keys = {
+			{
+				"<leader>?",
+				function()
+					require("which-key").show({ global = false })
+				end,
+				desc = "Buffer Local Keymaps (which-key)",
+			},
+		},
+	},
+}
