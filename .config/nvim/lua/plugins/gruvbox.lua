36fae8d3dcd5f3a53055078308674e7dbed97b21
MarcioSobel <marcinhosobel@hotmail.com>
2025-02-02 03:45:18 -0300

    Change gruvbox to the medium variant

--- .config/nvim/lua/plugins/gruvbox.lua
+++ .config/nvim/lua/plugins/gruvbox.lua
@@ -5,11 +5,11 @@ return {
 		terminal_colors = true,
 		lazy = false,
 		config = function()
-		  vim.g.gruvbox_material_foreground = "material"
-		  vim.g.gruvbox_material_background = "soft"
+			vim.g.gruvbox_material_foreground = "material"
+			vim.g.gruvbox_material_background = "medium"
 
-		  vim.g.gruvbox_material_better_performance = 1
-		  vim.cmd([[colorscheme gruvbox-material]])
+			vim.g.gruvbox_material_better_performance = 1
+			vim.cmd([[colorscheme gruvbox-material]])
 		end,
-	}
+	},
 }
