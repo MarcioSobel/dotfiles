2c839d301335b40d302dbacbfc5dbe264f739e45
MarcioSobel <marcinhosobel@hotmail.com>
2025-02-15 07:01:56 -0300

    customize telescope

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -3,28 +3,38 @@ return {
 		"nvim-telescope/telescope.nvim",
 		dependencies = {
 			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope-ui-select.nvim",
 		},
-		opts = {
-			defaults = {
-				prompt_prefix = "   ",
-				selection_caret = " ",
-				entry_prefix = " ",
-				sorting_strategy = "ascending",
-				layout_config = {
-					horizontal = {
-						prompt_position = "top",
-						preview_width = 0.55,
+		config = function()
+			local telescope = require("telescope")
+			telescope.setup({
+				defaults = {
+					prompt_prefix = "   ",
+					selection_caret = " ",
+					entry_prefix = " ",
+					sorting_strategy = "ascending",
+					layout_config = {
+						horizontal = {
+							prompt_position = "top",
+							preview_width = 0.55,
+						},
+						width = 0.87,
+						height = 0.80,
 					},
-					width = 0.87,
-					height = 0.80,
+					mappings = {
+						n = { ["q"] = require("telescope.actions").close },
+					},
+					-- default: { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+					borderchars = { "─", "│", "─", "│", "┌", "┐", "┘", "└" },
 				},
-				mappings = {
-					n = { ["q"] = require("telescope.actions").close },
-				},
-			},
 
-			extensions_list = { "themes", "terms" },
-			extensions = {},
-		},
+				extensions_list = { "themes", "terms" },
+				extensions = {
+					["ui-select"] = {},
+				},
+			})
+
+			telescope.load_extension("ui-select")
+		end,
 	},
 }
