ad28c98fe0afddec2e1039db157abd4cf158747b
MarcioSobel <marcinhosobel@hotmail.com>
2025-05-16 14:22:14 -0300

    Add icons to LSP diagnostics

--- .config/nvim/lua/config/vim.lua
+++ .config/nvim/lua/config/vim.lua
@@ -57,3 +57,43 @@ vim.cmd("augroup END")
 local sep = "/"
 local delim = ":"
 vim.env.PATH = table.concat({ vim.fn.stdpath("data"), "mason", "bin" }, sep) .. delim .. vim.env.PATH
+
+-- add icons to LSP diagnostics
+local severity_icons = {
+	[vim.diagnostic.severity.ERROR] = " ",
+	[vim.diagnostic.severity.WARN] = " ",
+	[vim.diagnostic.severity.INFO] = " ",
+	[vim.diagnostic.severity.HINT] = "󰌵 ",
+}
+
+local get_icon_from_diagnostic = function(diagnostic)
+	return severity_icons[diagnostic.severity] or "●"
+end
+
+vim.diagnostic.config({
+	severity_sort = true,
+	update_in_insert = true,
+	float = {
+		format = function(diagnostic)
+			local MAX_MESSAGE_LENGTH = 50
+			local message = diagnostic.message
+			if #message > MAX_MESSAGE_LENGTH then
+				message = message:sub(1, MAX_MESSAGE_LENGTH - 3) .. "..."
+			end
+			return string.format("%s %s", get_icon_from_diagnostic(diagnostic), message)
+		end,
+	},
+	virtual_text = {
+		prefix = get_icon_from_diagnostic,
+		spacing = 2,
+		align = "eol",
+	},
+})
+
+-- transparent background
+vim.cmd([[
+  highlight Normal guibg=none
+  highlight NonText guibg=none
+  highlight Normal ctermbg=none
+  highlight NonText ctermbg=none
+]])
